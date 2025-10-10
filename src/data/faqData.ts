26f6a6b609baece7824cb60b04a8277d21347258
Evan Thomas <74596543+PolarTaffy@users.noreply.github.com>
2025-10-10 08:34:39 -0400

    Switched To More Defined Colors

--- src/data/faqData.ts
+++ src/data/faqData.ts
@@ -1,64 +1,64 @@
 
 const faqData = {
-	tickets: {
-			townie: "Pierre",
-			portrait: "https://stardewvalleywiki.com/mediawiki/images/7/7e/Pierre.png",
-			greeting: "Welcome to my shop! ...Er, the event! I'm always happy to help a paying customer. What can I get for you?",
-			questions: [
-				{ q: "When is the registration deadline?", a: "Answer" },
-				{ q: "How many members can be in a group?", a: "Answer2" },
-				{ q: "Question3", a: "Answer3" }
-			]
+	general: {
+        townie: "Pierre",
+        portrait: "https://stardewvalleywiki.com/mediawiki/images/7/7e/Pierre.png",
+        greeting: "Welcome to my shop! ...Er, the hackathon! What can I do for you?",
+        questions: [
+            { q: "When is the registration deadline?", a: "Answer" },
+            { q: "How many members can be in a group?", a: "Answer2" },
+            { q: "Question3", a: "Answer3" }
+        ]
 	},
 	food: {
-			townie: "Gus",
-			portrait: "https://stardewvalleywiki.com/mediawiki/images/5/52/Gus.png",
-			greeting: "Welcome, friend! The Stardrop Saloon is proud to cater this event. I've got a little something for everyone. What are you thirsty for?",
-			questions: [
-				{ q: "Question 1", a: "Answer1" },
-				{ q: "Question 2", a: "Answer2" },
-				{ q: "Question 3", a: "Answer3" }
-			]
+        townie: "Gus",
+        portrait: "https://stardewvalleywiki.com/mediawiki/images/5/52/Gus.png",
+        greeting: "Welcome, friend! The Stardrop Saloon is proud to cater this hackathon. I've got a little something for everyone. What are you thirsty for?",
+        questions: [
+            { q: "Question 1", a: "Answer1" },
+            { q: "Question 2", a: "Answer2" },
+            { q: "Question 3", a: "Answer3" }
+        ]
 	},
 	venue: {
-			townie: "Mayor Lewis",
-			portrait: "https://stardewvalleywiki.com/mediawiki/images/2/2b/Lewis.png",
-			greeting: "Ah, hello there! It's important to keep things organized for our big event. I've got the official proclamations right here. What information do you require?",
-			questions: [
-				{ q: "Question 1", a: "Answer1" },
-				{ q: "Question 2", a: "Answer2" },
-				{ q: "Question 3", a: "Answer3" }
-			]
+        townie: "Mayor Lewis",
+        portrait: "https://stardewvalleywiki.com/mediawiki/images/2/2b/Lewis.png",
+        greeting: "Hello there, villager! I've got the official event proclamations right here. What information do you require?",
+        questions: [
+            { q: "Question 1", a: "Answer1" },
+            { q: "Question 2", a: "Answer2" },
+            { q: "Question 3", a: "Answer3" }
+        ]
 	},
-	rules: {
-			townie: "Marnie",
-			portrait: "https://stardewvalleywiki.com/mediawiki/images/5/52/Marnie.png",
-			greeting: "Oh, hello, dear! It's lovely to see you. Are you getting your things ready? It's always good to be prepared. Just... please don't bring any stray animals, alright? I have my hands full with my own!",
-			questions: [
-				{ q: "Question 1", a: "Answer1" },
-				{ q: "Question 2", a: "Answer2" },
-				{ q: "Question 3", a: "Answer3" }
-			]
+	items: {
+        townie: "Marnie",
+        portrait: "https://stardewvalleywiki.com/mediawiki/images/5/52/Marnie.png",
+        greeting: "Oh, hello, dear! It's lovely to see you. Are you getting your things ready? It's always good to be prepared. Just... please don't bring any stray animals, alright? I have my hands full with my own!",
+        questions: [
+            { q: "Question 1", a: "Answer1" },
+            { q: "Question 2", a: "Answer2" },
+            { q: "Question 3", a: "Answer3" }
+        ]
 	},
 	lost: {
-			townie: "Gunther",
-			portrait: "https://stardewvalleywiki.com/mediawiki/images/3/3d/Gunther.png",
-			greeting: "Greetings. While you are enjoying the festivities, should you happen to misplace an artifact... or, I suppose, a 'personal belonging,' please report it to the Museum Tent. I am overseeing the collection.",
-			questions: [
-				{ q: "Question 1", a: "Answer1" },
-				{ q: "Question 2", a: "Answer2" },
-				{ q: "Question 3", a: "Answer3" }
-			]
+        townie: "Gunther",
+        portrait: "https://stardewvalleywiki.com/mediawiki/images/3/3d/Gunther.png",
+        greeting: "Greetings. While you are enjoying the festivities, should you happen to misplace a 'personal belonging,' please report it to me. I am overseeing the collection.",
+        questions: [
+            { q: "Question 1", a: "Answer1" },
+            { q: "Question 2", a: "Answer2" },
+            { q: "Question 3", a: "Answer3" }
+        ]
 	},
-	special: {
-			townie: "Wizard",
-			portrait: "https://stardewvalleywiki.com/mediawiki/images/c/c7/Wizard.png",
-			greeting: "Ah... you have arrived, as the spirits foresaw. Your mind is clouded with questions. Speak, and I shall consult the ethereal planes for an answer.",
-			questions: [
-				{ q: "Question 1", a: "Answer1" },
-				{ q: "Question 2", a: "Answer2" },
-				{ q: "Question 3", a: "Answer3" }
-			]
+	misc: {
+        townie: "Wizard",
+        portrait: "https://stardewvalleywiki.com/mediawiki/images/c/c7/Wizard.png",
+        greeting: "Ah... you have arrived, as the spirits foresaw. Your mind is clouded with questions. Speak, and I shall consult the spirits for an answer.",
+        questions: [
+            { q: "Question 1", a: "Answer1" },
+            { q: "Question 2", a: "Answer2" },
+            { q: "Question 3", a: "Answer3" }
+        ]
 	}
 };
 
